63016db60a7f452e7f71bacdbaaef0b46432cd3b
Ken Kang <kenjen1617@gmail.com>
2025-03-21 18:44:25 -0400

    Arregle lo que angel me pidio, espero que funcione ahora

--- Calculadora1/Clases/CalculatorClass.js
+++ Calculadora1/Clases/CalculatorClass.js
@@ -1,32 +1,29 @@
- export default class  Calculator {
-    constructor (operand1Element, operand2Element,operand3Element){
+export default class Calculator {
+    constructor(operand1Element, operand2Element, operand3Element) {
         this.operand1Element = operand1Element;
         this.operand2Element = operand2Element;
         this.operand3Element = operand3Element;
         this.clear();
-
     }
 
-    clear(){
-        this.operand1 = 0;
+    clear() {
+        this.operand1 = "";
         this.operand2 = 0;
         this.operand3Element.innerHTML = '';
         this.operator = '';
         this.updateUI();
-        
-       
     }
 
-    updateUI(){
-        this.operand1Element.innerHTML = this.operand1 + this.operator;
+    updateUI() {
+        this.operand1Element.innerHTML = `${this.operand1} ${this.operator} ${this.operand2}`;
+        this.operand2Element.innerHTML = "";
+    }
+
+    appendNumber(number) {
+        if (number === "." && this.operand2.includes('.')) return;
+        this.operand2 = this.operand2 == 0 ? number : this.operand2.toString() + number;
         this.operand2Element.innerHTML = this.operand2;
-    
-
-    }
-
-    appendNumber(number){
-        if(number === "." && this.operand2.includes('.')) return;
-        this.operand2 = this.operand2 == 0 ? number: this.operand2.toString()+number;
+        
         this.updateUI();
     }
 
@@ -36,59 +33,53 @@
         this.updateUI();
     }
 
-
-    operation(operator){
-        if(this.operator){
+    operation(operator) {
+        if (this.operator) {
             this.Calc();
         }
         this.operator = operator;
         this.operand1 = this.operand2 === 0 ? this.operand1 : this.operand2;
-        this.operand2 = 0;
+        this.operand2 = "";
         this.updateUI();
     }
-    Calc(){
 
+    Calc() {
         console.log("operand1 despues", this.operand1);
         console.log("operand2 despues", this.operand2);
         console.log("operator", this.operator);
 
-        let num1 = parseFloat(this.operand1); // El valor de num1 va ser igual al resultado de "PARSEFLOAT" el cual es una 
-                                                // funcion que, me convierte el valor de OPERAND1 a FLOAT
+        let num1 = parseFloat(this.operand1);
         let num2 = parseFloat(this.operand2);
-        var operation = "";  // Es un STRING y esta vacia porque 
+        let operation = "";
+        let result = 0;
 
-
-        switch(this.operator){
+        switch (this.operator) {
             case "+":
-                this.operand2 = num1 + num2;
-               operation= `${num1}+${num2}`; // concatenamos(coge el valor de un valor externo y lo unimos al valor que queremos representar) 
-               // el resultado en un string 
-               //  donde la suma del num1 + num2 nos estaria
-                //nos estaria dando el resultado deseado
-               
+                result = num1 + num2;
+                operation = `${num1}+${num2} = ${result}`;
                 break;
             case "-":
-                this.operand2 = num1 - num2;
-                operation= `${num1}-${num2}`
+                result = num1 - num2;
+                operation = `${num1}-${num2} = ${result}`;
                 break;
             case "*":
-                this.operand2 = num1 * num2;
-                 operation= `${num1} * ${num2}`
+                result = num1 * num2;
+                operation = `${num1} * ${num2} = ${result}`;
                 break;
-            case "%":  
-                this.operand2 = num1 * num2 /100 ;
-                 operation= `${num1} % ${num2}`
+            case "%":
+                result = num1 * num2 / 100;
+                operation = `${num1} % ${num2} = ${result}`;
                 break;
             case "/":
-                this.operand2 = num1 / num2;
-                 operation= `${num1} / ${num2}`
+                result = num1 / num2;
+                operation = `${num1} / ${num2} = ${result}`;
                 break;
         }
-        this.operator= "";
 
+        this.operator = "";
+        this.operand2 = result;
         this.operand3Element.innerHTML = operation;
-        this.operand1 = "";
-       
+        this.operand1 = " ";
         this.updateUI();
     }
 }
